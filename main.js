58da533c6dffa579d2035941698ff73532ad7b1f
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-07-19 10:32:19 +0500

    Add modal

---
+++ main.js
@@ -0,0 +1,15 @@
+var elOpenModal = document.querySelector(".modal-open-btn");
+var elModal = document.querySelector(".modal");
+var elCloseModal = document.querySelector(".js-close-btn");
+
+elOpenModal.addEventListener("click", function(){
+    elModal.classList.add("open-modal")
+})
+
+elCloseModal.addEventListener("click", function(){
+    elModal.classList.remove("open-modal")
+})
+
+elModal.addEventListener("click", function(){
+    elModal.classList.remove("open-modal")
+})
